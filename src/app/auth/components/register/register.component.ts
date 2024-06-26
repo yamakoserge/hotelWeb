c54efc5f8f30e93a28f2bd29f95af5f5256bba8d
Serges YAMAKO <yamakoserges@gmail.com>
2024-06-26 16:41:59 +0100

    Auth Service & Calling Signup User API

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { AuthService } from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,13 +12,28 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class RegisterComponent {
  registerForm: any;
 
- constructor(private fb: FormBuilder,){}
+ constructor(private fb: FormBuilder,
+  private authService: AuthService,
+  private message: NzMessageService,
+  private router: Router
+){}
 
  ngOnInit(){
   this.registerForm =this.fb.group({
     email:[null, [Validators.email, Validators.required]],
     password: [null, Validators.required],
     name: [null, Validators.required]
+  })
+ }
+ submitForm(){
+  this.authService.register(this.registerForm.value).subscribe(res=>{
+    if(res.id !=null){
+      this.message.success("Signup successful", { nzDuration:3000});
+      this.router.navigateByUrl("/");
+
+    }else{
+      this.message.error(`${res.message}`, {nzDuration:3000})
+    }
   })
  }
 }
